206f0f45f80838f43f071a924c5b79b8ac818723
Samuel Weller <samweller24@gmail.com>
2020-03-02 20:18:39 -0500

    Initial Commit

---
+++ size.ai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  size.ai
+//
+//  Created by Sam Weller on 2020-03-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
