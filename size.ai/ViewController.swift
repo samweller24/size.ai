afbd0d4a2d1fd3c90d64bcdf58609a637cd11a06
Samuel Weller <samweller24@gmail.com>
2020-03-05 10:53:33 -0500

    Working Code Update 1
    updated code - allows for image upload and flask response
    
    small UI changes

--- size.ai/ViewController.swift
+++ size.ai/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imageView: UIImageView!
     var selectedImage: UIImage!
     var imagePicker: UIImagePickerController!
-
+    
+    //label to duaply data from server
+    @IBOutlet weak var estimateLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        estimateLabel.isHidden = true
+        let img = UIImage(named: "crowdTest.jpeg")
+        print("Calling function")
+        myImageUploadRequest(image: img!)
         // Do any additional setup after loading the view.
     }
+    
+    func displayPrediction(data: String){
+           estimateLabel.isHidden = false
+           dataLabel.text = data
+       }
 
     @IBAction func buttonHit(_ sender: UIButton) {
         let imagePicker = UIImagePickerController()
@@ -48,7 +61,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
          imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         self.selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-         self.uploadImage(Image: selectedImage!)
+        self.myImageUploadRequest(image: selectedImage!)
                
         picker.dismiss(animated: true, completion: nil)
     }
@@ -59,39 +72,119 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     
-    func uploadImage(Image: UIImage){
-        guard let image = Image.pngData() else{
-            print("error setting image")
-            return
-        }
+    func myImageUploadRequest(image: UIImage)
+           {
+             
+               let myUrl = NSURL(string: "http://127.0.0.1:5000/");
+               
+               let request = NSMutableURLRequest(url:myUrl! as URL);
+               request.httpMethod = "POST";
+               
+               let param = [
+                   "firstName"  : "Sam",
+                   "lastName"    : "Weller",
+                   "userId"    : "9"
+               ]
+               
+               let boundary = generateBoundaryString()
+               
+               request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+               
         
-        //creating network
-        let url = URL(string: "URL TO FLASK SERVER")
-        var request = URLRequest(url: url!)
+               let imageData = image.jpegData(compressionQuality: 1)
+               
+               if(imageData==nil)  { return; }
+               
+               request.httpBody = createBodyWithParameters(parameters: param, filePathKey: "file", imageDataKey: imageData! as NSData, boundary: boundary) as Data
+               
+               
+             
+               
+               let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                   data, response, error in
+                   
+                   if error != nil {
+                       print("error=\(error)")
+                       return
+                   }
+                   
+                   // You can print out response object
+                   print("******* response = \(response)")
+                   
+    
+                
+                   
+                   // Print out reponse body
+                   let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                   print("****** response data = \(responseString!)")
+    
+                    self.displayPrediction(data: responseString! as String)
+                   
+                   do {
+                       let json = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+                       
+                       print(json)
+                       
+                       
+                   }catch
+                   {
+                       print(error)
+                   }
+                   
+               }
+               
+               task.resume()
+           }
+           
+           
+           func createBodyWithParameters(parameters: [String: String]?, filePathKey: String?, imageDataKey: NSData, boundary: String) -> NSData {
+               let body = NSMutableData();
+               
+               if parameters != nil {
+                   for (key, value) in parameters! {
+                       body.appendString(string: "--\(boundary)\r\n")
+                       body.appendString(string: "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                       body.appendString(string: "\(value)\r\n")
+                   }
+               }
+              
+                       let filename = "user-profile.jpg"
+                       let mimetype = "image/jpg"
+                       
+               body.appendString(string: "--\(boundary)\r\n")
+               body.appendString(string: "Content-Disposition: form-data; name=\"\(filePathKey!)\"; filename=\"\(filename)\"\r\n")
+               body.appendString(string: "Content-Type: \(mimetype)\r\n\r\n")
+               body.append(imageDataKey as Data)
+               body.appendString(string: "\r\n")
+               
+           
+               
+               body.appendString(string: "--\(boundary)--\r\n")
+               
+               return body
+           }
+           
+           
+           
+           func generateBoundaryString() -> String {
+               return "Boundary-\(NSUUID().uuidString)"
+           }
+    
+
+    
         
-        //creating request
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.setValue("\(image.count)", forHTTPHeaderField: "Content-Length")
-        request.httpBody = image
-        
-        //execute request
-        let upload = URLSession.shared.uploadTask(with: request as URLRequest , from: image) { (data: Data?, response: URLResponse?, error: Error?) in
-            
-            if (error != nil){
-                print(error)
-                return
-            }
-            
-            
-            //code to be completed
-        }
-        
-        upload.resume();
         
         
-        
-    }
+       }
+       extension NSMutableData {
+          
+           func appendString(string: String) {
+               let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+               append(data!)
+           }
+
+
     
 }
+
 
