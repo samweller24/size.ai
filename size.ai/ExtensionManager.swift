f1e04d13546e83021c52eb6dfbdec9e00351a76d
Samuel Weller <samweller24@gmail.com>
2020-03-06 22:28:14 -0500

    UI tweaks
    overall changes to user interface

---
+++ size.ai/ExtensionManager.swift
@@ -0,0 +1,24 @@
+//
+//  ExtensionManager.swift
+//  size.ai
+//
+//  Created by Sam Weller on 2020-03-05.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = 1
+        
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+}
+ 
